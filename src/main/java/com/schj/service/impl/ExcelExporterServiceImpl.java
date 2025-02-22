727866244b17c5185fb01ab1f3a75c269aac27c0
AvA <14929406+ava_workspace@user.noreply.gitee.com>
2025-02-22 21:25:07 +0800

    feat: 保单信息导出到文件

--- src/main/java/com/schj/service/impl/ExcelExporterServiceImpl.java
+++ src/main/java/com/schj/service/impl/ExcelExporterServiceImpl.java
@@ -22,20 +22,33 @@ public class ExcelExporterServiceImpl implements ExcelExporterService {
     @Autowired
     private InsurancePolicyMapper insurancePolicyMapper;
 
+    /**
+     * 重写ExcelExporter方法，用于导出保险政策信息到Excel文件
+     * 此方法根据指定的数量从数据库获取保险政策信息，并将其写入到Excel文件中
+     * 如果目标Excel文件不存在，此方法将创建一个新的Excel文件
+     *
+     * @param count 指定从数据库获取保险政策信息的数量
+     */
     @Override
     public void ExcelExporter(int count) {
-
+        // 定义Excel文件的路径和名称
         File excelFile = new File("D:/Test/保单信息表.xlsx");
+        // 检查Excel文件是否存在，如果不存在则尝试创建它
         if (!excelFile.exists()) {
             try {
                 excelFile.createNewFile();
             } catch (IOException e) {
+                // 如果文件创建过程中发生IO异常，打印异常信息
                 e.printStackTrace();
             }
         }
 
+        // 从数据库获取指定数量的保险政策信息列表
         List<InsurancePolicy> list = insurancePolicyMapper.getInsurancePolicyList(count);
 
+        // 使用EasyExcel库将保险政策信息列表写入Excel文件中
+        // 这里指定Excel文件、数据模型类、工作表名称和要写入的数据列表
         EasyExcel.write(excelFile, InsurancePolicy.class).sheet("保单信息").doWrite(list);
     }
+
 }
